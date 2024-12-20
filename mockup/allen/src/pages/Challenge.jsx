1226d6f4272ae97daf275cf4ea2c90c5c3e9d6ce
Allenyzh <allenyangzhenhao@gmail.com>
2024-12-20 18:08:32 -0500

    Finished Page

--- mockup/allen/src/pages/Challenge.jsx
+++ mockup/allen/src/pages/Challenge.jsx
@@ -1,24 +1,82 @@
-import { SendIcon } from "../components/Icons";
+import { UserIcon, BotIcon, SendIcon } from "../components/Icons";
 import { useOutletContext } from "react-router-dom";
+import { useState } from "react";
 
 export default function Challenge() {
-  const { sendMessage, handleChange, userInput } = useOutletContext();
+  const { messages, setMessages } = useOutletContext();
+
+  const [userInput, setUserInput] = useState("");
+
+  function sendMessage() {
+    if (userInput.trim() !== "") {
+      setMessages([...messages, { text: userInput, isUser: true }]);
+      setUserInput("");
+      console.log(messages);
+    }
+  }
+
+  function handleChange(e) {
+    setUserInput(e.target.value);
+  }
+
   return (
-    <>
-      <textarea
-        className="border border-slate-950 min-h-8 max-h-24 w-96 pt-1 pl-1 rounded tracking-wider"
-        placeholder="Here we go..."
-        value={userInput}
-        onChange={handleChange}
-      />
-      <div className="px-3 flex">
-        <button
-          className="hover:scale-110 active:scale-95"
-          onClick={sendMessage}
-        >
-          <SendIcon />
-        </button>
+    <div className="flex-1 flex flex-col">
+      {/* 对话区域 */}
+      <div className="flex-1 rounded-tr-lg shadow-lg bg-gray-100 pt-8">
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`flex ${
+              message.isUser ? "justify-end" : "justify-start"
+            }`}
+          >
+            <div
+              className={`items-center py-1 flex ${
+                message.isUser ? "pr-2 flex-row-reverse" : "pl-2 flex-row"
+              }`}
+            >
+              <div className={`${message.isUser ? "pl-2" : "pr-2"}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="undefined"
+                >
+                  {message.isUser ? <UserIcon /> : <BotIcon />}
+                </svg>
+              </div>
+              <div
+                className={`px-4 py-2 rounded-lg first-letter:uppercase ${
+                  message.isUser
+                    ? "bg-blue-500 text-white"
+                    : "bg-gray-200 text-gray-800"
+                }`}
+              >
+                {message.text}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-    </>
+      {/* 输入框 */}
+      <div className="max-h-24 p-3 flex justify-center items-center shadow-inner bg-gray-200 rounded-l-none rounded-br-lg">
+        <textarea
+          className="border border-slate-950 min-h-8 max-h-24 w-96 pt-1 pl-1 rounded tracking-wider"
+          placeholder="Here we go..."
+          value={userInput}
+          onChange={handleChange}
+        />
+        <div className="px-3 flex">
+          <button
+            className="hover:scale-110 active:scale-95"
+            onClick={sendMessage}
+          >
+            <SendIcon />
+          </button>
+        </div>
+      </div>
+      {/* ------------- */}
+    </div>
   );
 }
--- mockup/allen/src/pages/layouts/ChallengeLayout.jsx
+++ mockup/allen/src/pages/layouts/ChallengeLayout.jsx
@@ -4,7 +4,6 @@ import { Outlet } from "react-router";
 import { useParams } from "react-router-dom";
 import scenarioData from "../../data/scenarios.json";
 import ErrorPage from "../../components/ErrorPage";
-import { UserIcon, BotIcon } from "../../components/Icons";
 
 export default function ChallengeLayout() {
   const scenarios = scenarioData;
@@ -12,22 +11,11 @@ export default function ChallengeLayout() {
   const currentId = parseInt(params.scenarioId); // 此处小坑，url中的参数是字符，必须转换为数字以后才能用于筛选
   const currentScenario = scenarios.filter((item) => item.id === currentId); // filter 返回的是[{...}]，如果需要返回{}可使用find
 
-  const [userInput, setUserInput] = useState("");
+  // const [userInput, setUserInput] = useState("");
   const [messages, setMessages] = useState([]);
 
   if (!params.scenarioId || isNaN(currentId) || !currentScenario[0]) {
     return <ErrorPage />;
-  }
-
-  function sendMessage() {
-    if (userInput.trim() !== "") {
-      setMessages([...messages, { text: userInput, isUser: true }]);
-      setUserInput("");
-    }
-  }
-
-  function handleChange(e) {
-    setUserInput(e.target.value);
   }
 
   const buttonAnimation =
@@ -39,66 +27,21 @@ export default function ChallengeLayout() {
       <div className="flex flex-1 pt-24 pb-2 max-w-7xl mx-auto">
         <div className="flex flex-1">
           {/* 左侧区域 */}
-          <div className="w-1/3 text-wrap flex flex-col border border-gray-950">
+          <div className="w-1/3 text-wrap flex flex-col bg-gray-200 rounded-lg rounded-r-none shadow-inner">
             <div className="font-semibold m-4">{currentScenario[0].title}</div>
-            <div className="min-h-96 m-4 border border-gray-950">
+            <div className="min-h-96 m-4 shadow-lg rounded-lg bg-white">
               <div className="p-5">{currentScenario[0].description}</div>
             </div>
             <Link className="m-auto" to="/">
               <button
-                className={`w-16 h-8 rounded bg-slate-400 text-white ${cardInnerButton} ${buttonAnimation}`}
+                className={`w-16 h-8 rounded bg-slate-400 text-white shadow-lg ${cardInnerButton} ${buttonAnimation}`}
               >
                 Back
               </button>
             </Link>
           </div>
           {/* 右侧区域 */}
-          <div className="flex-1 flex flex-col">
-            {/* 对话区域 */}
-            <div className="flex-1 border-y border-e border-gray-950">
-              {messages.map((message, index) => (
-                <div
-                  key={index}
-                  className={`flex ${
-                    message.isUser ? "justify-end" : "justify-start"
-                  }`}
-                >
-                  <div
-                    className={`items-center py-1 flex ${
-                      message.isUser ? "pr-2 flex-row-reverse" : "pl-2 flex-row"
-                    }`}
-                  >
-                    <div className={`${message.isUser ? "pl-2" : "pr-2"}`}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24px"
-                        viewBox="0 -960 960 960"
-                        width="24px"
-                        fill="undefined"
-                      >
-                        {message.isUser ? <UserIcon /> : <BotIcon />}
-                      </svg>
-                    </div>
-                    <div
-                      className={`px-4 py-2 rounded-lg first-letter:uppercase ${
-                        message.isUser
-                          ? "bg-blue-500 text-white"
-                          : "bg-gray-200 text-gray-800"
-                      }`}
-                    >
-                      {message.text}
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-            {/* 输入框 */}
-            <div className="max-h-24 p-3 flex justify-center items-center border-e border-b border-gray-900">
-              {/*  */}
-              <Outlet context={{ sendMessage, handleChange, userInput }} />
-            </div>
-            {/* ------------- */}
-          </div>
+          <Outlet context={{ messages, setMessages }} />
         </div>
       </div>
     </>
