ad89dd0020cf569908ee4d62ed71279d3de97e34
Allenyzh <allenyangzhenhao@gmail.com>
2024-12-23 16:57:35 -0500

    Update Navbar.jsx

--- mockup/allen/src/components/Navbar.jsx
+++ mockup/allen/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { UserIcon } from "./Icons";
-
+import { Link } from "react-router";
 /* eslint-disable react/prop-types */
 export default function Navbar({ pageName }) {
   return (
     <>
       <nav className="fixed top-0 w-screen bg-slate-200 shadow-md z-50 dark:bg-gray-800 dark:text-white select-none">
         <div className="max-w-7xl mx-auto flex justify-between items-center p-5 md:px-8">
-          <div className="font-bold text-xl">MTLove</div>
+          <Link to="/" className="font-bold text-xl hover:scale-110">
+            MTLove
+          </Link>
           {/* Page name */}
           <div className="flex text-center items-center font-bold capitalize text-2xl">
             {pageName || "Scenario"}
