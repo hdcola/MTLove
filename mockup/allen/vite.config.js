d8de5b07223f0d1f78f2036168169bb7730ebf7e
Allenyzh <allenyangzhenhao@gmail.com>
2025-01-03 14:49:15 -0500

    State management & Componentization
    1.Used Zustand for messages' states management
    2.Componentized dialogue display function
    3.Fixed related bugs
    4.Drank plenty of water to keep hydrated

--- mockup/allen/vite.config.js
+++ mockup/allen/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "mtlove",
+  base: "/mtlove",
   server: {
     // host: true, // 启用局域网访问
     // port: 5173, // 默认端口（可自定义）
