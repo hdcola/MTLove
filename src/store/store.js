4d99eff192ce4bc42bd8165a024bd6427deba74a
hdcola <gh@hdcola.org>
2025-02-01 18:36:36 -0500

    use env get api key

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const api = "";
+const api = import.meta.env.VITE_API_KEY;
 
 export const useStore = create(
   persist(
@@ -9,15 +9,15 @@ export const useStore = create(
       apiKey: api,
       //   setApiKey: (newKey) => set({ apiKey: newKey }),
 
-      userInput: "",
+      userInput: '',
       setUserInput: (input) => set({ userInput: input }),
 
-      systemPrompt: "",
+      systemPrompt: '',
       setSystemPrompt: (prompt) => set({ systemPrompt: prompt }),
 
       messages: [
         {
-          role: "system",
+          role: 'system',
           content:
             "Right now, your role is my girlfriend, and we need to play a game, the rules is: I have to convince you to do something that you don't want to do, and you are responding according to my response. You will give an interger score whether it is positive or negative from -10 to 10 for each response.",
         },
@@ -47,29 +47,29 @@ export const useStore = create(
         } = get();
         if (!userInput) return;
 
-        addMessage({ role: "user", content: userInput }); // 添加用户对话
-        setUserInput(""); // 清空输入框
+        addMessage({ role: 'user', content: userInput }); // 添加用户对话
+        setUserInput(''); // 清空输入框
 
         const requestBody = {
-          model: "gemini-2.0-flash-exp",
-          messages: [...messages, { role: "user", content: userInput }],
+          model: 'gemini-2.0-flash-exp',
+          messages: [...messages, { role: 'user', content: userInput }],
           response_format: {
-            type: "json_schema",
+            type: 'json_schema',
             json_schema: {
-              name: "compliance_result",
+              name: 'compliance_result',
               schema: {
-                type: "object",
+                type: 'object',
                 properties: {
                   text: {
-                    type: "string",
-                    description: "Response text.",
+                    type: 'string',
+                    description: 'Response text.',
                   },
                   score: {
-                    type: "number",
-                    description: "Score of the response.",
+                    type: 'number',
+                    description: 'Score of the response.',
                   },
                 },
-                required: ["text", "score"],
+                required: ['text', 'score'],
                 additionalProperties: false,
               },
               strict: true,
@@ -82,9 +82,9 @@ export const useStore = create(
         const response = await fetch(
           `https://generativelanguage.googleapis.com/v1beta/openai/chat/completions`,
           {
-            method: "POST",
+            method: 'POST',
             headers: {
-              "Content-Type": "application/json",
+              'Content-Type': 'application/json',
               Authorization: `Bearer ${apiKey}`,
             },
             body: JSON.stringify(requestBody),
@@ -100,9 +100,9 @@ export const useStore = create(
         const curScore = jsonObj.score;
         setScore(curScore);
         setScoreHistory(curScore);
-        addMessage({ role: "assistant", content: jsonObj.text });
+        addMessage({ role: 'assistant', content: jsonObj.text });
       },
     }),
-    { name: "mtlove" }
+    { name: 'mtlove' }
   )
 );
