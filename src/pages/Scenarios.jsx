110d6237d12ad0ce15ab8eb6ed1b1f4184f7846a
Allenyzh <allenyangzhenhao@gmail.com>
2025-02-05 16:19:10 -0500

    Update Scenarios.jsx

--- src/pages/Scenarios.jsx
+++ src/pages/Scenarios.jsx
@@ -4,7 +4,7 @@ import "../index.css";
 export default function Scenarios() {
   return (
     <section className="h-dvh w-dvw flex flex-col justify-scenter items-center">
-      <div className="fixed top-0 md:top-4 left-1/2 transform -translate-x-1/2 bg-white/30 backdrop-blur-sm shadow-lg md:rounded-2xl px-28 md:px-96 py-8 md:py-12 flex flex-col justify-center items-center gap-4 z-50 mtlove">
+      <div className="fixed top-0 md:top-4 left-1/2 transform -translate-x-1/2 bg-white/30 backdrop-blur-sm shadow-lg md:rounded-2xl px-[255px] lg:px-[555px] md:px-[355px] sm:px-[300px] py-8 md:py-12 flex flex-col justify-center items-center gap-4 z-50 mtlove">
         <h1 className="text-5xl font-bold text-blue-600">MTLove</h1>
         <div className="w-24 h-1 bg-blue-500 mx-auto rounded-full"></div>
       </div>
